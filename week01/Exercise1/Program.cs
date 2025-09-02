b26b1e274b90e38b2feeb56e5309acabb3961e42
Christopher Bahinting <bahintingchristopher@gmail.com>
2025-09-02 16:38:31 +0800

    completing the exercise 1

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -3,7 +3,17 @@ using System;
 class Program
 {
     static void Main(string[] args)
+
+
+    // completing my output. 
+    
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.Write("What is your first name? ");
+        string first_name = Console.ReadLine();
+        Console.Write("What is you last name? ");
+        string last_name = Console.ReadLine();
+     
+        Console.WriteLine($"Your name is {last_name}, {first_name} {last_name} ");
+
     }
 }
