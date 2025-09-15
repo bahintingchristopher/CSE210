d6258c71ce636a70756167200459ab096f0e93bc
Christopher Bahinting <bahintingchristopher@gmail.com>
2025-09-15 13:40:26 +0800

    fraction project initial commint

---
+++ week03/Fractions/Fraction.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Security.Cryptography.X509Certificates;
+
+public class Fraction
+{
+    private int _top;
+    private int _bottom;
+
+    public Fraction()
+    {
+        _top = 1;
+        _bottom = 1;
+    }
+
+    public Fraction(int wholeNumber)
+    {
+        _top = wholeNumber;
+        _bottom = 1;
+    }
+
+    public Fraction(int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom;
+    }
+    public string GetFractionString()
+    {   
+        string answer = ($"{_top}/{_bottom}");
+        return answer;
+    }
+
+    public double GetDecimalValue()
+    {
+        return (double)_top / _bottom;
+    }
+}
+    
+        
+
