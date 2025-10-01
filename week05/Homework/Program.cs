48c1226d92dcfa0eb1b5ecaaf12fd8338a4a2a75
Christopher Bahinting <bahintingchristopher@gmail.com>
2025-10-01 22:47:15 +0800

    WEEK5 HOMEWORK

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -5,5 +5,13 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Homework Project.");
+
+        Console.WriteLine();
+        Assignment a1 = new Assignment("Roberto Rodriguez", "Fractions");
+        Console.WriteLine(a1.GetSummary());
     }
+
+    
+
+
 }
