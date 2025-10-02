0ff4e8679e9977bcce205dc8e4fe6644cc5f5a3a
Christopher Bahinting <bahintingchristopher@gmail.com>
2025-10-02 12:29:48 +0800

    LEARNING PRACTICE WEEK5

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -7,11 +7,22 @@ class Program
         Console.WriteLine("Hello World! This is the Homework Project.");
 
         Console.WriteLine();
-        Assignment a1 = new Assignment("Roberto Rodriguez", "Fractions");
+        //create base class assignment
+        Assignment a1 = new Assignment("SAMUEL BENNETT", "Multiplication");
         Console.WriteLine(a1.GetSummary());
+
+        //create derived class math assignment
+        MathAssignment m1 = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(m1.GetSummary());
+        Console.WriteLine(m1.GetHomeworkList());
+
+        WritingAssignment w1 = new WritingAssignment("Christoher Bahinting", "European History", "The Causes of World War II");
+        Console.WriteLine(w1.GetSummary());
+        Console.WriteLine(w1.GetWritingInformation());
     }
+
+}
 
     
 
 
-}
